8857e3111c897b5697f9111384c55220614a24cc
Theis Agerskov Tengs <theistengs@gmail.com>
2020-09-28 11:58:48 +0200

    Yes Anders

--- src/App.js
+++ src/App.js
@@ -1,26 +1,64 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import ReactDOM from "react-dom";
+import "./App.css";
+import ReactAudioPlayer from "react-audio-player";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+let songPath = "";
+
+function songSwitcher(song) {
+  if (song === "all_my_love") {
+    songPath = "./Music/all_my_love.mp3";
+    console.log({ songPath });
+  } else if (song === "blinding_lights") {
+    songPath = "Music/blinding_lights.mp3";
+    console.log({ songPath });
+  } else if (song === "in_your_eyes") {
+    songPath = "Music/in_your_eyes.mp3";
+    console.log({ songPath });
+  } else {
+    alert("Not a real song?");
+  }
+}
+
+class App extends Component {
+  state = {
+    music: "audio",
+  };
+  render() {
+    return (
+      <div>
+        <h2>Streaming Service</h2>
+        <ReactAudioPlayer
+          ref={(ref) => (this.music = ref)}
+          src="all_my_love.mp3"
+          autoPlay
+          controls
+        />
+
+        <button
+          type="button"
+          onClick={() => songSwitcher("all_my_love")}
+          className="btn btn-primary"
         >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+          All My Love
+        </button>
+        <button
+          type="button"
+          onClick={() => songSwitcher("blinding_lights")}
+          className="btn btn-primary"
+        >
+          Blinding Lights
+        </button>
+        <button
+          type="button"
+          onClick={() => songSwitcher("in_your_eyes")}
+          className="btn btn-primary"
+        >
+          Robin Schulz - In Your Eyes
+        </button>
+      </div>
+    );
+  }
 }
 
 export default App;
